661271ff3799b09344517a1ee4c89de4ee7dd42a
Eugene <ti.eugene@gmail.com>
2020-11-18 20:57:27 +0300

    core.forms.DeleteMultiForm works

--- core/forms.py
+++ core/forms.py
@@ -7,16 +7,22 @@ from django.utils.translation import gettext as _
 from . import models
 
 
-class MultiFileAddForm(forms.Form):
-    file = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True}), label=_('File'))
-
-
-class FileDeleteMultiForm(forms.Form):
-    checked = forms.ModelMultipleChoiceField(queryset=models.File.objects.all(),
+class DeleteMultiForm(forms.Form):
+    checked = forms.ModelMultipleChoiceField(queryset=None,
                                              widget=forms.CheckboxSelectMultiple(), label=_("Selected"))
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         checked = kwargs['initial'].get('checked')
         if checked:
-            self.fields['checked'].queryset = models.File.objects.filter(pk__in=checked)
+            self.fields['checked'].queryset = self.model.objects.filter(pk__in=checked)
+        else:
+            self.fields['checked'].queryset = self.model.objects.all()
+
+
+class MultiFileAddForm(forms.Form):
+    file = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True}), label=_('File'))
+
+
+class FileDeleteMultiForm(DeleteMultiForm):
+    model = models.File
